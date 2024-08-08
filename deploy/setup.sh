fcd22efc7489a8e6f00b40c6905f503fbe1b6e00
Miles <juniornyamile@gmail.com>
2024-08-08 13:17:29 +0000

    Configured again for deploymenttt

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -12,8 +12,8 @@ locale-gen en_GB.UTF-8
 
 # Install Python, SQLite and pip
 echo "Installing dependencies..."
-apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
+sudo apt-get update
+sudo apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
